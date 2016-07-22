ac5cf93b47f820860136ac1abe58be6e18c6f1c1
oliv2776 <olivierdeschenes2776@gmail.com>
2016-07-22 12:23:38 +0200

    V4

--- ClientPC.cpp
+++ ClientPC.cpp
@@ -5,28 +5,54 @@
 *\version 0.1
 */
 
+#if defined (WIN32)
+typedef int socklen_t;
 #include "stdafx.h"
 /*To avoid _WINSOCK_DEPRECATED_NO_WARNINGS error while compiling*/
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
-#include <iostream>
 #include <winsock2.h>
+#include <iostream>
+#include <fstream>
+#pragma comment(lib, "ws2_32.lib")
+using namespace std;
+
+#elif defined (linux)
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#define INVALID_SOCKET -1
+#define SOCKET_ERROR -1
+#define closesocket(s) close(s)
+typedef int SOCKET;
+typedef struct sockaddr_in SOCKADDR_IN;
+typedef struct sockaddr SOCKADDR;
+#endif
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
 
-#pragma comment(lib, "ws2_32.lib")
 
-typedef int socklen_t;
-using namespace std;
+
 
 #define PORT 23
 
 int main()
 {
-	WSADATA WSAData;
-	int error = WSAStartup(MAKEWORD(2,2), &WSAData);
 	SOCKET sock;
 	SOCKADDR_IN sin;
+	char buffer[32] = "";
+	char buf;
+#if defined (WIN32)
+	WSADATA WSAData;
+	int error = WSAStartup(MAKEWORD(2, 2), &WSAData);
+#else
+	int error = 0;
+#endif
 
 	if (!error) {
 		/*Create the socket*/
@@ -38,17 +64,38 @@ int main()
 		sin.sin_port = htons(PORT);
 
 		/*If the client connect*/
-		if (connect(sock, (SOCKADDR*)&sin, sizeof(sin)) != SOCKET_ERROR)
+		if (connect(sock, (SOCKADDR*)&sin, sizeof(sin)) != SOCKET_ERROR) 
+		{
 			printf("Connecting to %s on the port %d\n", inet_ntoa(sin.sin_addr), htons(sin.sin_port));
+			/*If we receive a message we display it on the screen*/
+			do {
+				printf("receiving data!\n");
+			
+				switch (buffer[0]) {
+				case 1:
+					printf("start adc1");
+					break;
+				default:
+					printf("%s\n", buffer);
+					break;
+					}
+
+				
+			} while (recv(sock, buffer, 32, 0) !=0);
+		}
 		else {
 			printf("impossible to connect\n");
-			while (1);
 		}
 
 		/*Closing the socket*/
 		closesocket(sock);
 
+#if defined (WIN32)
 		WSACleanup();
+#endif
 	}
+	/*Waiting for the user to type something to close*/
+	getchar();
+
 	return EXIT_SUCCESS;
 }
