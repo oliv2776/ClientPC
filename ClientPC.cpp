fc033a1a7f76a68afdf44f2ac80d6ea7efc154d4
oliv2776 <olivierdeschenes2776@gmail.com>
2016-07-22 13:32:47 +0200

    V5

--- ClientPC.cpp
+++ ClientPC.cpp
@@ -35,18 +35,30 @@ typedef struct sockaddr SOCKADDR;
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
+#include <time.h>
 
-
-
-
+/*Defines*/
 #define PORT 23
+#define BOARDNAME 'B0'
 
 int main()
 {
 	SOCKET sock;
 	SOCKADDR_IN sin;
 	char buffer[32] = "";
-	char buf;
+	/*Signals sent: buffer[0]= board ID; buffer[1]=ADC number */
+	char bufferADC1[32], bufferADC2[32], bufferADC3[32];
+	int timeConversion;
+	SYSTEMTIME t;
+	GetSystemTime(&t);
+
+	/*Configuring the buffers*/
+	bufferADC1[0] = BOARDNAME;
+	bufferADC2[0] = BOARDNAME;
+	bufferADC3[0] = BOARDNAME;
+	bufferADC1[1] = 'a1';
+	bufferADC2[1] = 'a2';
+	bufferADC2[1] = 'a3';
 #if defined (WIN32)
 	WSADATA WSAData;
 	int error = WSAStartup(MAKEWORD(2, 2), &WSAData);
@@ -67,21 +79,54 @@ int main()
 		if (connect(sock, (SOCKADDR*)&sin, sizeof(sin)) != SOCKET_ERROR) 
 		{
 			printf("Connecting to %s on the port %d\n", inet_ntoa(sin.sin_addr), htons(sin.sin_port));
+			printf("time: %dh %dmin %dsec\n",t.wHour,t.wMinute,t.wSecond);
 			/*If we receive a message we display it on the screen*/
-			do {
-				printf("receiving data!\n");
-			
-				switch (buffer[0]) {
-				case 1:
-					printf("start adc1");
-					break;
-				default:
-					printf("%s\n", buffer);
-					break;
-					}
+			while (buffer[0] != 'z') {
+				if (recv(sock, buffer, 32, 0) != 0) {
+					printf("receiving data!\n");
 
-				
-			} while (recv(sock, buffer, 32, 0) !=0);
+					switch (buffer[0]) {
+					case 'a':
+						printf("start adc1\n");
+						timeConversion = buffer[1];
+						printf("The conversion will last %dmin\n", timeConversion);
+						buffer[0] = 0;
+						bufferADC1[3] = timeConversion;
+						for (int i = 0; i < timeConversion; i++) {
+							bufferADC1[4] = i;
+							send(sock, bufferADC1, 32, 0);
+						}
+						break;
+					case 'b':
+						printf("start adc2\n");
+						timeConversion = buffer[1];
+						printf("The conversion will last %dmin\n", timeConversion);
+						buffer[0] = 0;
+						bufferADC2[3] = timeConversion;
+						for (int i = 0; i < timeConversion; i++) {
+							bufferADC2[4] = i;
+							send(sock, bufferADC2, 32, 0);
+						}
+						break;
+					case 'c':
+						printf("start adc3\n");
+						timeConversion = buffer[1];
+						printf("The conversion will last %dmin\n", timeConversion);
+						buffer[0] = 0;
+						bufferADC3[3] = timeConversion;
+						for (int i = 0; i < timeConversion; i++) {
+							bufferADC3[4] = i;
+							send(sock, bufferADC3, 32, 0);
+						}
+						break;
+
+					default:
+						printf("%s\n", buffer);
+						break;
+					}
+				}
+			}
+			
 		}
 		else {
 			printf("impossible to connect\n");
