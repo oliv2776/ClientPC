8ba02e3ffcd6fd99e20b0e6b44a3f0af2cf342b3
oliv2776 <olivierdeschenes2776@gmail.com>
2016-09-01 15:24:18 +0200

    V?

--- ClientPC.cpp
+++ ClientPC.cpp
@@ -40,31 +40,42 @@ typedef struct sockaddr SOCKADDR;
 
 /*Defines*/
 #define PORT 23
-#define BOARDNAME1 'A'
-#define BOARDNAME2 '0'
+#define BOARDNAME 1
 #define ADC1 '1'
 #define ADC2 '2'
 #define ADC3 '3'
 #define SIZEDATA 1024
-#define SIZETRAME SIZEDATA+30
+#define SIZEFRAME 2*SIZEDATA+21
 
-int formatbuffer(char buffer[],int nbPacket, int numberTotalOfPackets);
+
+union frame_u {
+	struct frame_s {
+		uint8_t board;
+		uint8_t adc_number;
+		uint32_t packet_number;
+		uint32_t total_of_packet;
+		uint8_t day;
+		uint8_t month;
+		uint8_t year;
+		uint8_t hour;
+		uint8_t minutes;
+		uint8_t seconds;
+		uint8_t miliseconds;
+		uint32_t data_lenght;
+		uint16_t data[SIZEDATA];
+	}frame_as_field;
+	char frame_as_byte[SIZEFRAME];
+};
+
+
+void formatbuffer(union frame_u frame, uint8_t adcnumber, uint32_t nbPacket, uint32_t numberTotalOfPackets);
 /**
 * @brief convert a table of chars into int
 * @param char charToConvert[] the table to convert
 * @param int index: the index in the table of char where there is the lengh of the number to convert
 * @retval uint_32 return the error code: 0= no errors; 1= wrong switch numbe
 */
-uint32_t convertCharToInt(char charToConvert[], int index);
 
-/**
-* @brief convert a Int into a table of char
-* @param char ConvertedInt[]: the table where the int will be put into
-* @param int indexTable: the index in the table of char where there is the lengh of the number to convert
-* @param uint32_t numberToConvert: the number to convert in the table of char
-* @retval void
-*/
-void convertIntToChar(char ConvertedInt[], int indexTable, uint32_t numberToConvert);
 
 int main()
 {
@@ -72,11 +83,7 @@ int main()
 	SOCKADDR_IN sin;
 	SYSTEMTIME t;
 	GetSystemTime(&t);
-	char bufferServer[32] = "";// buffer for the commandes sent by the server
-	/*Signals sent: buffer[0]= board ID; buffer[1]=ADC number; 
-	buffer[2] = packet number; buffer[3] = total number of packets; 
-	buffer[4] = day; buffer[5] = month; buffer[6]= year; buffer[7]= hour; 
-	buffer[8]= min; buffer[9]= sec; buffer[10] = milliseconds*/
+	char bufferServer[SIZEFRAME] = "";
 	char bufferADC1[32], bufferADC2[32], bufferADC3[32];
 	char tempBuffer[SIZEDATA];
 	for (int i = 0; i < SIZEDATA; i++) {
@@ -84,6 +91,8 @@ int main()
 	}
 	uint32_t timeConversion;
 	int index;
+
+	union frame_u *frame_buf;
 
 #if defined (WIN32)
 	WSADATA WSAData;
@@ -113,50 +122,15 @@ int main()
 				if (recv(sock, bufferServer, sizeof(bufferServer), 0) != 0) {
 					printf("receiving data!\n");
 
-					switch (bufferServer[0]) {
-					case 'a':
-						printf("start adc1\n");
-						timeConversion = convertCharToInt(bufferServer, 1);
-						printf("The conversion will last %dmin\n", timeConversion);
-						for (int i = 0; i < timeConversion; i++) {
-							index = formatbuffer(bufferADC1,i, timeConversion);
-							for (int j = 0; j < SIZEDATA; j++) {
-								bufferADC1[j + index] = tempBuffer[j];
-							}
-							for (int k = 0; k < SIZETRAME; k++) {
-								printf("%c\t", bufferADC1);
-								printf("\n");
-							}
-
-						}
-						
-						
-						break;
-					case 'b':
-						printf("start adc2\n");
-						timeConversion = bufferServer[1];
-						printf("The conversion will last %dmin\n", timeConversion);
-						bufferADC2[3] = timeConversion;
-						for (int i = 0; i < timeConversion; i++) {
-							bufferADC2[4] = i;
-							send(sock, bufferADC2, 32, 0);
-						}
-						break;
-					case 'c':
-						printf("start adc3\n");
-						timeConversion = bufferServer[1];
-						printf("The conversion will last %dmin\n", timeConversion);
-						bufferADC3[3] = timeConversion;
-						for (int i = 0; i < timeConversion; i++) {
-							bufferADC3[4] = i;
-							send(sock, bufferADC3, 32, 0);
-						}
-						break;
-
-					default:
-						printf("%s\n", bufferServer);
-						break;
+					if (bufferServer[1] == 1) {
+						printf("Start ADC1\n");
 					}
+
+					for (int i = 0; i < SIZEFRAME; i++) {
+						printf("%d\t", bufferServer[i]);
+					}
+					
+					
 				}
 			}
 			
@@ -166,7 +140,7 @@ int main()
 		}
 
 		/*Closing the socket*/
-		closesocket(sock);
+		//closesocket(sock);
 
 #if defined (WIN32)
 		WSACleanup();
@@ -178,126 +152,23 @@ int main()
 	return EXIT_SUCCESS;
 }
 
- int formatbuffer(char buffer[], int nbPacket,int numberTotalOfPackets) {
+ void formatbuffer(union frame_u frame, uint8_t adcnumber, uint32_t nbPacket, uint32_t numberTotalOfPackets) {
 	SYSTEMTIME t;
 	GetSystemTime(&t);
-	/*Configuring the buffers*/
-	buffer[0] = BOARDNAME1;
-	buffer[1] = BOARDNAME2;
-	buffer[2] = ADC1;
-	int index = 3;
-	char buf[6];
-	convertIntToChar(buf, 0, nbPacket);
-	for (int i = 0; i < 6; i++) {
-		printf("%c", buf[i]);
-		buffer[index] = buf[i];
-		index++;
-	}
+
+	frame.frame_as_field.board = BOARDNAME;
+	frame.frame_as_field.adc_number = adcnumber;
+	frame.frame_as_field.packet_number = nbPacket;
+	frame.frame_as_field.total_of_packet = numberTotalOfPackets;
+	frame.frame_as_field.day = t.wDay;
+	frame.frame_as_field.month = t.wMonth;
+	frame.frame_as_field.year = t.wYear;
+	frame.frame_as_field.hour = t.wHour;
+	frame.frame_as_field.minutes = t.wMinute;
+	frame.frame_as_field.seconds = t.wSecond;
+	frame.frame_as_field.miliseconds = t.wMilliseconds;
 	
-	convertIntToChar(buf, 0, numberTotalOfPackets);
-	for (int i = 0; i < 6; i++) {
-		printf("%c", buf[i]);
-		buffer[index] = buf[i];
-		index++;
-	}
 
-	convertIntToChar(buf, 0, t.wDay);
-	for (int i = 0; i < 2; i++) {
-		buffer[index] = buf[i];
-		index++;
-	}
-
-	convertIntToChar(buf, 0, t.wMonth);
-	for (int i = 0; i < 2; i++) {
-		buffer[index] = buf[i];
-		index++;
-	}
-
-	convertIntToChar(buf, 0, t.wYear);
-	for (int i = 0; i < 4; i++) {
-		buffer[index] = buf[i];
-		index++;
-	}
-
-	convertIntToChar(buf, 0, t.wHour);
-	for (int i = 0; i < 2; i++) {
-		buffer[index] = buf[i];
-		index++;
-	}
-
-	convertIntToChar(buf, 0, t.wMinute);
-	for (int i = 0; i < 2; i++) {
-		buffer[index] = buf[i];
-		index++;
-	}
-
-	convertIntToChar(buf, 0, t.wSecond);
-	for (int i = 0; i < 2; i++) {
-		buffer[index] = buf[i];
-		index++;
-	}
-
-	convertIntToChar(buf, 0, t.wMilliseconds);
-	for (int i = 0; i < 2; i++) {
-		buffer[index] = buf[i];
-		index++;
-	}
-	return index;
+	
 }
 
- 
- uint32_t convertCharToInt(char charToConvert[], int index)
- {
-	 uint32_t result = 0;
-	 uint32_t temp = 0;
-
-	 int newIndex = charToConvert[index] - 48;
-	 /*Reconverting chars into int*/
-	 for (int i = index + 1; i < index + 1 + newIndex; i++) {
-		 charToConvert[i] = charToConvert[i] - 48;
-	 }
-	 printf("\n\n%d\n", index);
-	 printf("\n\n%d\n", newIndex);
-	 /*converting the char[] numbers into 1 int */
-	 for (int i = 0; i < index + 1 + charToConvert[index] - 48; i++)
-	 {
-		 temp = charToConvert[index + i + 1];
-
-		 for (int j = 0; j < newIndex - 2; j++)
-		 {
-			 temp = temp * 10;
-		 }
-		 newIndex--;
-		 result += temp;
-	 }
-	 return result;
- }
-
-
-
- void convertIntToChar(char ConvertedInt[], int indexTable, uint32_t numberToConvert) {
-	 uint32_t modulo;
-	 int newIndex = 0;
-	 char tempChar[16];
-
-	 do {
-		 modulo = numberToConvert % 10;
-		 tempChar[newIndex] = modulo + 48;
-		 newIndex++;
-		 numberToConvert /= 10;
-	 } while (numberToConvert);
-
-	 for (int i = 0; i<newIndex / 2; i++)
-	 {
-		 int tampon = tempChar[i];
-		 tempChar[i] = tempChar[newIndex - 1 - i];
-		 tempChar[newIndex - 1 - i] = tampon;
-	 }
-
-
-	 for (int i = 0; i < newIndex; i++) {
-		 ConvertedInt[i + indexTable + 1] = tempChar[i];
-	 }
-	 ConvertedInt[indexTable] = newIndex + 48;
-
- }
